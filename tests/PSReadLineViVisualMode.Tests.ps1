df6c84f4f8965bbb74a5bda6e6e0c6b02fed1fab
MarcoBuess <42165023+MarcoBuess@users.noreply.github.com>
2021-07-16 11:56:25 +0200

    Renamed Invoke-UserInputGenerator.ps1 to Get-Generators.ps1 to later consolidate generators in that file.
    Refactored pester test to meet pester 5.x standards

--- tests/PSReadLineViVisualMode.Tests.ps1
+++ tests/PSReadLineViVisualMode.Tests.ps1
@@ -1,39 +1,39 @@
-$ModuleName = $MyInvocation.MyCommand.Name.Split('.')[0]
-$ModulePath = "$((Get-Item -Path $MyInvocation.MyCommand.Path).Directory.parent.FullName)\src\"
-$Module = $ModulePath + $ModuleName
+. $PSScriptRoot/Get-Generators.ps1
 
-Get-Module -Name $Module -ListAvailable -All | Remove-Module -Force -ErrorAction Ignore
+BeforeAll {
+    $ModuleName = $([System.IO.Path]::GetFileName($PSCommandPath)).Split('.')[0]
+    $ModulePath = "$((Get-Item -Path $PSCommandPath).Directory.parent.FullName)\src\"
+    $Module = $ModulePath + $ModuleName
 
-Import-Module -Name $Module -Force -ErrorAction Stop
+    Get-Module -Name $Module -ListAvailable -All | Remove-Module -Force -ErrorAction Ignore
+    Import-Module -Name $Module -Force -ErrorAction Stop
+}
 
-ls $PSScriptRoot\generators | select -exp FullName | % { . $_ }
+Describe "Test-Input" {
+    It "When a single motion <UserInput> is entered <Expected> is returned." -TestCases @(
+        @{ UserInput = 'w'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'w'} }
+        @{ UserInput = 'v'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'v'} }
+        @{ UserInput = 'e'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'e'} }
+        @{ UserInput = 'l'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'l'} }
+    ) {
+        param ($UserInput, $Expected)
 
-InModuleScope $ModuleName {
-    Describe "Test-Input" {
-        It "When a single motion <UserInput> is entered <Expected> is returned." -TestCases @(
-            @{ UserInput = 'w'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'w'} }
-            @{ UserInput = 'v'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'v'} }
-            @{ UserInput = 'e'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'e'} }
-            @{ UserInput = 'l'; Expected = [PSCustomObject]@{motionCount = 1; motion = 'l'} }
-        ) {
-            param ($UserInput, $Expected)
+        (Compare-Object -ReferenceObject $Expected `
+                        -DifferenceObject $(Test-Input -UserInput $UserInput) `
+                        -IncludeEqual).SideIndicator
+        | Should -Be "=="
+    }
 
-            (Compare-Object -ReferenceObject $Expected `
-                           -DifferenceObject $(Test-Input -UserInput $UserInput) `
-                           -IncludeEqual).SideIndicator
-            | Should -Be "=="
-        }
+    It "When a multi motion <UserInput> is entered <Expected> is returned." -TestCases $(Invoke-UserInputGenerator -Samples 100) {
+        param ($UserInput, $Expected)
 
-        It "When a multi motion <UserInput> is entered <Expected> is returned." -TestCases $(Invoke-UserInputGenerator -Samples 100) {
-            param ($UserInput, $Expected)
-
-            (Compare-Object -ReferenceObject $Expected `
-                           -DifferenceObject $(Test-Input -UserInput $UserInput) `
-                           -IncludeEqual).SideIndicator
-            | Should -Be "=="
-        }
+        (Compare-Object -ReferenceObject $Expected `
+                        -DifferenceObject $(Test-Input -UserInput $UserInput) `
+                        -IncludeEqual).SideIndicator
+        | Should -Be "=="
     }
 }
 
-Get-Module -Name $Module -ListAvailable -All `
-| Remove-Module -Force -ErrorAction Ignore
+AfterAll {
+    Get-Module -Name $ModuleName -ListAvailable -All | Remove-Module -Force -ErrorAction Ignore
+}
